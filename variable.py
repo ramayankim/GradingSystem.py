29af1c2eddd1a97b1385975a8bb3e4d7746c4d74
ramayankim <ramayan.kim22@gmail.com>
2026-02-13 19:51:40 +0800

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,17 @@
+fullname ="Leo Rayos"
+age = 20
+height = 5.6
+currently_working = False
+
+print("Fullname:", fullname)
+print(type(fullname))
+
+print("Age:", age)
+print(type(age))
+
+print("Height in meters:", height)
+print(type(height))
+
+print("Currently working?", currently_working)
+print(type(currently_working))
+
